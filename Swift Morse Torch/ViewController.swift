8f655defd41cb5d1b790c5baaf78f9f9db2e5db0
John Clem <johnnyclem@gmail.com>
2014-06-05 14:08:08 -0700

    add empty Xcode project

---
+++ Swift Morse Torch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Morse Torch
+//
+//  Created by John Clem on 6/5/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
